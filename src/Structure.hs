03afd25fec208319c409030ebcaf92b839bc6c47
AnfiNa <anfilatova.75@yandex.ru>
2026-02-17 13:08:24 +0300

    refactoring fix

---
+++ src/Structure.hs
@@ -0,0 +1,15 @@
+module Structure
+  ( PrefixTreeDict,
+    emptyDict,
+    insertDict,
+    deleteDict,
+    lookupDict,
+    memberDict,
+    filterDict,
+    mergeDict,
+    foldlWithKey,
+    foldrWithKey,
+  )
+where
+
+import Structure.Internal
